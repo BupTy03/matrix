61c74a2ba943d9c21371b4f12a8a46a7933b634a
BupTy03 <41854435+BupTy03@users.noreply.github.com>
2018-07-29 17:27:57 +0500

    slice added

--- MyMatrix/SMatrix.h
+++ MyMatrix/SMatrix.h
@@ -1,162 +1,562 @@
 #pragma once
 
 #include<iostream>
+#include<algorithm>
 #include<exception>
-#include<utility>
+#include<string>
+#include<initializer_list>
 
 using Index = long;
 
 using namespace std;
 
-struct SMatrix_error {
+struct Matrix_error {
 	string name;
-	SMatrix_error(const char* q) :name(q) { }
-	SMatrix_error(string n) :name(move(n)) { }
+	Matrix_error(const char* q) : name(q) {}
+	Matrix_error(string s) :name(s) {}
 	string what() { return name; }
 };
 
-inline void error(const char* p)
+inline void error(const char* p) { throw Matrix_error(p); }
+
+template<typename T>
+T Clamp(T& x, const T& lo, const T& hi)
 {
-	throw SMatrix_error(p);
+	T result = x;
+	if (x < lo) result = lo;
+	else if (x > hi) result = hi;
+	return x = result;
 }
 
-template<typename T, const Index d1, const Index d2>
-class SMatrix
+
+template<typename T>
+class Matrix
 {
 private:
-	T elem[d1][d2] = {};	// usual 2-dim array
-	const Index sz = d1 * d2;	// count whole elements
+	T * * data;
+	Index dm1;
+	Index dm2;
+	Index sz;
+	Index space_d1;
+	Index space_d2;
 
 public:
-	constexpr SMatrix() noexcept {}
-	constexpr SMatrix(const T& val)
+	Matrix() noexcept : data(nullptr), dm1(0), dm2(0), sz(0), space_d1(0), space_d2(0) {}
+	Matrix(const Index x, const Index y) : dm1(x), dm2(y), sz(dm1 * dm2), space_d1(dm1), space_d2(dm2)
 	{
-		for (Index i = 0; i < d1; i++)
-			for (Index j = 0; j < d2; j++)
-				elem[i][j] = val;
+		if (dm1 <= 0 || dm2 <= 0)
+			error("Invalid argument for Matrix<T>::Matrix(const Index, const Index)");
+		//throw invalid_argument("Invalid argument for Matrix<T>::Matrix(const Index, const Index)");
+
+		data = new T*[dm1];
+
+		for (Index i = 0; i < dm1; i++)
+			data[i] = new T[dm2]();
+
 	}
-	// construct matrix from the array(size of array should be d1*d2)
-	template<const Index n>
-	constexpr SMatrix(const T(&arr)[n])
+	Matrix(const Index x, const Index y, const T& val) : Matrix(x, y)
 	{
-		static_assert(d1*d2 == n, "size of Matrix != size of array");
-
-		Index num = 0;
-		for (Index i = 0; i < d1; ++i)
-		{
-			for (Index j = 0; j < d2; ++j)
-			{
-				elem[i][j] = arr[num];
-				num++;
-			}
-		}
+		for (Index i = 0; i < dm1; i++)
+			for (Index j = 0; j < dm2; j++)
+				data[i][j] = val;
 	}
-	// construct matrix from the braced initializer
-	SMatrix(const std::initializer_list<T> init_list)
+	Matrix(const Matrix& other) : dm1(other.dm1), dm2(other.dm2), sz(other.sz), space_d1(other.space_d1), space_d2(other.space_d2)
 	{
-		if (init_list.size() != this->sz)
-			error("invalid argument for constructor: size of initializer_list is not equal to the size of matrix");
-			//throw invalid_argument{ "Invalid argument for constructor SMatrix<T>::SMatrix(std::initializer_list<T>)" };
-
-
-		auto initlstIt = init_list.begin();
-		for (Index i = 0; i < d1; ++i) 
-		{
-			for (Index j = 0; j < d2; ++j) 
-			{
-				elem[i][j] = *initlstIt;
-				initlstIt++;
-			}
-		}
+		for (Index i = 0; i < dm1; ++i)
+			for (Index j = 0; j < dm2; ++j)
+				data[i][j] = other.data[i][j];
 	}
-	
-	SMatrix(const SMatrix& other)
+	Matrix(Matrix&& other) : Matrix()
 	{
-		std::copy(other.cbegin(), other.cend(), this->begin());
+		swap(data, other.data);
+		swap(dm1, other.dm1);
+		swap(dm2, other.dm2);
+		swap(sz, other.sz);
+		swap(space_d1, other.space_d1);
+		swap(space_d2, other.space_d2);
 	}
-	SMatrix& operator =(const SMatrix&) = default;
 
-	SMatrix(SMatrix&&) = delete;	//	should not be moved
-	SMatrix& operator =(SMatrix&&) = delete;
+	Matrix& operator=(const Matrix&) = default;
+	Matrix& operator =(Matrix&&) = default;
 
-	constexpr Index size_dim1() const { return d1; }	// returns number of elements in a row
-	constexpr Index size_dim2() const { return d2; }	// returns number of elements in a column
-	constexpr Index size() const { return sz; }
-
-	// pointer to the data
-	T* data() { return (T*)elem; }
-	const T* data() const { return (const T*)elem; }
+	Index size_dim1() const noexcept { return dm1; }
+	Index size_dim2() const noexcept { return dm2; }
+	Index size() const noexcept { return sz; }
 
 	void range_check(Index i, Index j)
 	{
-		if (i < 0 || i >= d1)
+		if (i < 0 || i >= dm1)
 			error("range error: dimention 1");
-		if (j < 0 || j >= d2)
+		if (j < 0 || j >= dm2)
 			error("range error: dimention 2");
 	}
 
-	// subscripting:
-	const T& operator ()(const Index i, const Index j) const
+	T const& operator ()(const Index i, const Index j) const
 	{
 		range_check(i, j);
-		return elem[i][j];
+		return data[i][j];
 	}
-	T& operator ()(const Index i, const Index j)
+	T& operator ()(Index i, Index j)
 	{
 		range_check(i, j);
-		return elem[i][j];
+		return data[i][j];
 	}
 
-	// slicing
-	T const* operator [](Index i) const { return elem[i]; }
-	T* operator [](Index i) { return elem[i]; }
+	T const* operator [](Index i) const { return data[i]; }
+	T* operator [](Index i) { return data[i]; }
+
+	// rows [n:dm1)
+	Matrix slice(Index n)
+	{
+		/*if (n < 0) n = 0;
+		else if (n >= dm1) n = dm1 - 1;*/
+		Clamp(n, (Index)0, dm1 - 1);
+
+		const Index newsz = dm1 - n;
+		Matrix M(newsz, dm2);
+		for (Index i = 0, k = n; i < newsz; ++i, ++k)
+		{
+			for (Index j = 0; j < dm2; ++j)
+			{
+				M[i][j] = this->data[k][j];
+			}
+		}
+		return M;
+	}
+	// rows [n:dm1)
+	const Matrix slice(Index n) const
+	{
+		/*if (n < 0) n = 0;
+		else if (n >= dm1) n = dm1 - 1;*/
+		Clamp(n, (Index)0, dm1 - 1);
+
+		const Index newsz = dm1 - n;
+		Matrix M(newsz, dm2);
+		for (Index i = 0, k = n; i < newsz; ++i, ++k)
+		{
+			for (Index j = 0; j < dm2; ++j)
+			{
+				M[i][j] = this->data[k][j];
+			}
+		}
+		return M;
+	}
+	//	rows [n:m)
+	Matrix slice(Index n, Index m)
+	{
+		/*if (n < 0) n = 0;
+		else if (n >= dm1) n = dm1 - 1;
+		if (m < 0) m = 0;
+		else if (m >= dm1) m = dm1 - 1;*/
+		Clamp(n, (Index)0, dm1 - 1);
+		Clamp(m, (Index)0, dm1 - 1);
+
+		if (m <= n) error("wrong slice");
+
+		const Index newsz = m - n + 1;
+		Matrix M(newsz, dm2);
+
+		for (Index i = 0, k = n; i < newsz; ++i, ++k)
+			for (Index j = 0; j < dm2; ++j)
+				M[i][j] = this->data[k][j];
+
+		return M;
+	}
+	//	rows [n:m)
+	const Matrix slice(Index n, Index m) const
+	{
+		/*if (n < 0) n = 0;
+		else if (n >= dm1) n = dm1 - 1;
+		if (m < 0) m = 0;
+		else if (m >= dm1) m = dm1 - 1;*/
+		Clamp(n, (Index)0, dm1 - 1);
+		Clamp(m, (Index)0, dm1 - 1);
+
+		if (m <= n) error("wrong slice");
+
+		const Index newsz = m - n + 1;
+		Matrix M(newsz, dm2);
+
+		for (Index i = 0, k = n; i < newsz; ++i, ++k)
+			for (Index j = 0; j < dm2; ++j)
+				M[i][j] = this->data[k][j];
+
+		return M;
+	}
+	// rows and colls [n1:m1) and [n2:m2)
+	Matrix slice(Index n1, Index n2, Index m1, Index m2)
+	{
+		Clamp(n1, (Index)0, dm1 - 1);
+		Clamp(m1, (Index)0, dm1 - 1);
+		Clamp(n2, (Index)0, dm2 - 1);
+		Clamp(m2, (Index)0, dm2 - 1);
+
+		if (m1 <= n1 || m2 <= n2) error("wrong slice");
+
+		const Index newsz_1 = m1 - n1 + 1;
+		const Index newsz_2 = m2 - n2 + 1;
+		Matrix M(newsz_1, newsz_2);
+
+		for (Index i = 0, a = n1; i < newsz_1; ++i, ++a)
+			for (Index j = 0, b = n2; j < newsz_2; ++j, ++b)
+				M[i][j] = this->data[a][b];
+
+		return M;
+	}
+	// rows and colls [n1:m1) and [n2:m2)
+	const Matrix slice(Index n1, Index n2, Index m1, Index m2) const
+	{
+		Clamp(n1, (Index)0, dm1 - 1);
+		Clamp(m1, (Index)0, dm1 - 1);
+		Clamp(n2, (Index)0, dm2 - 1);
+		Clamp(m2, (Index)0, dm2 - 1);
+
+		if (m1 <= n1 || m2 <= n2) error("wrong slice");
+
+		const Index newsz_1 = m1 - n1 + 1;
+		const Index newsz_2 = m2 - n2 + 1;
+		Matrix M(newsz_1, newsz_2);
+
+		for (Index i = 0, a = n1; i < newsz_1; ++i, ++a)
+			for (Index j = 0, b = n2; j < newsz_2; ++j, ++b)
+				M[i][j] = this->data[a][b];
+
+		return M;
+	}
 
 	void fill(const T& val)
 	{
-		for (Index i = 0; i < d1; i++)
-			for (Index j = 0; j < d2; j++)
-				elem[i][j] = val;
+		for (Index i = 0; i < dm1; i++)
+			for (Index j = 0; j < dm2; j++)
+				data[i][j] = val;
 	}
 
-	// applies the function to each of the elements
 	template<typename F, typename... Args>
 	void apply(F func, Args&&... args)
 	{
-		for (Index i = 0; i < d1; i++)
-			for (Index j = 0; j < d2; j++)
+		for (Index i = 0; i < dm1; i++)
+			for (Index j = 0; j < dm2; j++)
 				data[i][j] = func(data[i][j], std::forward<Args>(args)...);
 	}
 
-	// output to the stream
-	friend ostream& operator <<(ostream& os, const SMatrix<T, d1, d2>& m)
+	void reserve_d1(Index newalloc)
 	{
-		os << "{ ";
-		for (Index i = 0; i < d1; i++)
+		if (newalloc <= space_d1) return;
+
+		T** p = new T*[newalloc];
+
+		for (Index i = 0; i < dm1; ++i)
+			p[i] = data[i];
+
+		delete[] data;
+		data = p;
+		space_d1 = newalloc;
+	}
+	Index capacity_d1() const { return space_d1; }
+	void resize_d1(Index newsize)
+	{
+		if (newsize <= dm1) return;
+
+		reserve_d1(newsize);
+		for (Index i = dm1; i < newsize; ++i)
+			data[i] = new T[space_d2]();
+
+		dm1 = newsize;
+		sz = dm1 * dm2;
+	}
+	void add_d1(const T& val)
+	{
+		if (space_d1 == 0)
+			reserve_d1(8);
+		else if (dm1 == space_d1)
+			reserve_d1(2 * space_d1);
+
+		data[dm1] = new T[space_d2];
+
+		for (Index i = 0; i < dm2; i++)
+			data[dm1][i] = val;
+
+		++dm1;
+		sz = dm1 * dm2;
+	}
+	template<typename Iter>
+	void add_d1(Iter first, Iter last)
+	{
+		if (std::distance(first, last) != dm2)
+			return;
+
+		if (space_d1 == 0)
+			reserve_d1(8);
+		else if (dm1 == space_d1)
+			reserve_d1(2 * space_d1);
+
+		data[dm1] = new T[space_d2];
+
+		for (Index i = 0; i < dm2; i++)
+		{
+			data[dm1][i] = *first;
+			first++;
+		}
+
+		++dm1;
+		sz = dm1 * dm2;
+	}
+	template<int n>
+	void add_d1(const T(&arr)[n])
+	{
+		if (n != dm2)
+			error("size of array is not equal to size of dimention1");
+
+		if (space_d1 == 0)
+			reserve_d1(8);
+		else if (dm1 == space_d1)
+			reserve_d1(2 * space_d1);
+
+		data[dm1] = new T[space_d2];
+
+		for (Index i = 0; i < n; ++i)
+			data[dm1][i] = arr[i];
+
+		++dm1;
+		sz = dm1 * dm2;
+	}
+
+	void reserve_d2(Index newalloc)
+	{
+		if (newalloc <= space_d2) return;
+
+		for (Index i = 0; i < dm1; i++)
+		{
+			T* p = new T[newalloc];
+
+			for (Index j = 0; j < dm2; j++)
+				p[j] = data[i][j];
+
+			delete[] data[i];
+			data[i] = p;
+		}
+
+		space_d2 = newalloc;
+	}
+	Index capacity_d2() const { return space_d2; }
+	void resize_d2(Index newsize)
+	{
+		if (newsize <= dm2) return;
+
+		reserve_d2(newsize);
+		for (Index i = 0; i < dm1; i++)
+		{
+			for (Index j = dm2; j < newsize; j++)
+				data[i][j] = {};
+		}
+
+		dm2 = newsize;
+		sz = dm1 * dm2;
+	}
+	void add_d2(const T& val)
+	{
+		if (space_d2 == 0)
+			reserve_d2(8);
+		else if (dm2 == space_d2)
+			reserve_d2(2 * space_d2);
+
+		for (Index i = 0; i < dm1; i++)
+			data[i][dm2] = val;
+
+		++dm2;
+		sz = dm1 * dm2;
+	}
+	template<typename Iter>
+	void add_d2(Iter first, Iter last)
+	{
+		if (std::distance(first, last) != dm1)
+			return;
+
+		if (space_d2 == 0)
+			reserve_d2(8);
+		else if (dm2 == space_d2)
+			reserve_d2(2 * space_d2);
+
+		for (Index i = 0; i < dm1; ++i)
+		{
+			data[i][dm2] = *first;
+			first++;
+		}
+
+		++dm2;
+		sz = dm1 * dm2;
+	}
+	template<int n>
+	void add_d2(const T(&arr)[n])
+	{
+		if (n != dm1)
+			error("size of array is not equal to size of dimention2");
+
+		if (space_d2 == 0)
+			reserve_d2(8);
+		else if (dm2 == space_d2)
+			reserve_d2(2 * space_d2);
+
+		for (Index i = 0; i < n; ++i)
+			data[i][dm2] = arr[i];
+
+		++dm2;
+		sz = dm1 * dm2;
+	}
+
+	void del_d1() { del_d1(dm1 - 1); }
+	void del_d1(Index num)
+	{
+		if (num < 0 || num >= dm1)
+			error("Index is outside of Matrix");
+
+		delete data[num];
+
+		--dm1;
+
+		for (Index i = num; i < dm1; ++i)
+			std::swap(data[i], data[i + 1]);
+
+		sz = dm1 * dm2;
+	}
+
+	void del_d2() { del_d2(dm2 - 1); }
+	void del_d2(Index num)
+	{
+		if (num < 0 || num >= dm2)
+			error("Index is outside of Matrix");
+
+		--dm2;
+
+		for (Index i = 0; i < dm1; ++i)
+			for (Index j = num; j < dm2; ++j)
+				std::swap(data[i][j], data[i][j + 1]);
+
+		sz = dm1 * dm2;
+	}
+
+	friend ostream& operator <<(ostream& os, const Matrix& m)
+	{
+		os << "{" << endl;
+		for (Index i = 0; i < m.dm1; i++)
 		{
 			os << "{ ";
-			for (Index j = 0; j < d2; j++)
+			for (Index j = 0; j < m.dm2; j++)
 			{
 				os << m[i][j];
 
-				if (j != d2 - 1)
+				if (j != m.dm2 - 1)
 					os << ", ";
 			}
 			os << " }";
 
-			if (i != d1 - 1)
-				os << ", \n  ";
+			if (i != m.dm1 - 1)
+				os << ", ";
+
+			os << endl;
 		}
-		os << " }";
+		os << "}";
 
 		return os;
 	}
 
-	using iterator = T * ;
-	using const_iterator = T const*;
+	class MatrixIterator : public std::iterator<std::input_iterator_tag, T>
+	{
+		friend class Matrix;
+	private:
+		MatrixIterator(T** p, Index s) : p(p), step(s - 1), left(0) {}
+	public:
+		MatrixIterator(const MatrixIterator &it) : p(it.p), step(it.step), left(it.left) {}
+		MatrixIterator& operator =(const MatrixIterator&) = default;
 
-	iterator begin() { return (T*)elem; }
-	iterator end() { return (T*)elem + sz; }
+		bool operator!=(MatrixIterator const& other) const { return p != other.p || left != other.left || step != other.step; }
+		bool operator==(MatrixIterator const& other) const { return p == other.p && left == other.left && step == other.step; }
+		T& operator*() const { return *(*p + left); }
+		MatrixIterator& operator++()
+		{
+			if (left >= step)
+			{
+				left = 0;
+				++p;
+				return *this;
+			}
+			left++;
+			return *this;
+		}
+		MatrixIterator& operator++(int)
+		{
+			if (left >= step)
+			{
+				left = 0;
+				++p;
+				return *this;
+			}
+			left++;
+			return *this;
+		}
+	private:
+		T * * p;
+		Index step;
+		Index left;
+	};
 
-	const_iterator cbegin() const { return (T const*)elem; }
-	const_iterator cend() const { return (T const*)elem + sz; }
+	class ConstMatrixIterator : public std::iterator<std::input_iterator_tag, T>
+	{
+		friend class Matrix;
+	private:
+		ConstMatrixIterator(T** p, Index s) : p(const_cast<const T**>(p)), step(s - 1), left(0) {}
+	public:
+		ConstMatrixIterator(const ConstMatrixIterator &it) : p(it.p), step(it.step), left(it.left) {}
+		ConstMatrixIterator& operator =(const ConstMatrixIterator&) = default;
+
+		bool operator!=(ConstMatrixIterator const& other) const { return p != other.p || left != other.left || step != other.step; }
+		bool operator==(ConstMatrixIterator const& other) const { return p == other.p && left == other.left && step == other.step; }
+		T const& operator*() const { return *(*p + left); }
+		ConstMatrixIterator& operator++()
+		{
+			if (left >= step)
+			{
+				left = 0;
+				++p;
+				return *this;
+			}
+			left++;
+			return *this;
+		}
+		ConstMatrixIterator& operator++(int)
+		{
+			if (left >= step)
+			{
+				left = 0;
+				++p;
+				return *this;
+			}
+			left++;
+			return *this;
+		}
+	private:
+		const T** p;
+		Index step;
+		Index left;
+	};
+
+	using iterator = MatrixIterator;
+	using const_iterator = ConstMatrixIterator;
+
+	iterator begin() { return iterator(data, dm2); }
+	iterator end() { return iterator(data + dm1, dm2); }
+
+	const_iterator cbegin() { return const_iterator(data, dm2); }
+	const_iterator cend() { return const_iterator(data + dm1, dm2); }
+
+	~Matrix()
+	{
+		if (data == nullptr)
+			return;
+
+		for (Index i = 0; i < dm1; i++)
+			delete[] data[i];
+
+		delete[] data;
+	}
 };
